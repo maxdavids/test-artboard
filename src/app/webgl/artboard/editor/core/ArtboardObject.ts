ad21c5b3104464869a5f03ed1a195fd99ca4b73a
mdavids <maximiliano.davids@gmail.com>
2021-02-09 20:22:36 -0300

    Removing legacy code.

--- src/app/webgl/artboard/editor/core/ArtboardObject.ts
+++ src/app/webgl/artboard/editor/core/ArtboardObject.ts
@@ -19,9 +19,6 @@ export default class ArtboardObject implements ISerializable {
     protected _components: IComponent[];
     protected _objects: ArtboardObject[];
 
-    protected _mainContainer: PIXI.Container;
-    protected _componentsContainer: PIXI.Container;
-
     protected _lastUpdateTime: number = 0;
 
     public attributes: Attributes;
@@ -32,15 +29,7 @@ export default class ArtboardObject implements ISerializable {
         this._components = [];
         this._objects = [];
 
-        this._mainContainer = new PIXI.Container();
-        this.addChildAt( this._mainContainer, 0 );
-
-        this._componentsContainer = new PIXI.Container();
-        this._mainContainer.addChild( this._componentsContainer );
-
         this.attributes = artboardObjectDef.attributes;
-
-        this.reset();
     }
 
     public serialize(): ArtboardObjectDef {
@@ -122,18 +111,6 @@ export default class ArtboardObject implements ISerializable {
             this._components.splice( atIndex, 0, component );
         }
 
-        if ( this.isDisplayable( component ) ) {
-            const displayObject: DisplayObject = component.getDisplayObject();
-
-            if ( !lastDisplayable ) {
-                this._componentsContainer.addChild( displayObject );
-
-            } else {
-                const displayIndex: number = this._componentsContainer.getChildIndex( lastDisplayable.getDisplayObject() );
-                this._componentsContainer.addChildAt( displayObject, displayIndex );
-            }
-        }
-
         component.onAdded();
     }
 
@@ -141,12 +118,6 @@ export default class ArtboardObject implements ISerializable {
         const index: number = this._components.indexOf( component );
         if ( index >= 0 ) {
             this._components.splice( index, 1 );
-
-            if ( this.isDisplayable( component ) ) {
-                const displayObject: DisplayObject = component.getDisplayObject();
-                this._componentsContainer.removeChild( displayObject );
-            }
-
             component.onRemoved();
 
             return true;
@@ -168,12 +139,10 @@ export default class ArtboardObject implements ISerializable {
     }
 
     public getObjectById( objectId: string ): ArtboardObject {
-        // Is it me?
         if ( this._id === objectId ) {
             return this;
         }
 
-        // It is not me, Is it one of my children?
         for ( const object of this._objects ) {
             const foundObject = object.getObjectById( objectId );
             if ( foundObject ) {
@@ -181,13 +150,11 @@ export default class ArtboardObject implements ISerializable {
             }
         }
 
-        // Nope, it is not even one of them...
         return undefined;
     }
 
     public addArtboardObject( object: ArtboardObject ): void {
         this._objects.push( object );
-        this._mainContainer.addChild( object );
     }
 
     public removeArtboardObject( object: ArtboardObject ): boolean {
@@ -195,8 +162,6 @@ export default class ArtboardObject implements ISerializable {
 
         if ( index >= 0 ) {
             this._objects.splice( index, 1 );
-            this._mainContainer.removeChild( object );
-
             return true;
         }
 
@@ -208,22 +173,11 @@ export default class ArtboardObject implements ISerializable {
         if ( oldIndex >= 0 && newIndex >= 0 && newIndex < this._objects.length && oldIndex !== newIndex ) {
             this._objects.splice( oldIndex, 1 );
             this._objects.splice( newIndex, 0, object );
-
-            this._mainContainer.setChildIndex( object, newIndex + 1 );
         }
     }
 
     public getObjectIndex( object: ArtboardObject ) {
         return this._objects.indexOf( object );
-    }
-
-    public reset(): void {
-        this.scale.set( this.attributes.scaleX, this.attributes.scaleY );
-        this.rotation = this.attributes.rotation;
-        this.alpha = this.attributes.alpha;
-
-        this.x = this.attributes.x;
-        this.y = this.attributes.y;
     }
 
     public updateComponents( globalElapsedMillis: number ): void {
@@ -241,7 +195,6 @@ export default class ArtboardObject implements ISerializable {
     public update( globalElapsedMillis: number ): void {
         this._lastUpdateTime = globalElapsedMillis;
 
-        this.reset();
         this.updateComponents( globalElapsedMillis );
         this.updateChildren( globalElapsedMillis );
     }
